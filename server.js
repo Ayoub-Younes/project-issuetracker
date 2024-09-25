c9aad64b72c016cc5ba2fe870a25cb27550a4b66
Ayoub-Younes <ayoub.jonah01@gmail.com>
2024-09-25 13:07:07 +0200

    Adjust testing

--- server.js
+++ server.js
@@ -37,3 +37,5 @@ app.use(function (req, res, next) {
 const listener = app.listen(process.env.PORT || 3000, function () {
     console.log('Your app is listening on port ' + listener.address().port);
 });
+
+module.exports = app; 
